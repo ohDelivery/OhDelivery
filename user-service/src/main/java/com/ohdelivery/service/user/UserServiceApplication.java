3d3ed0bcb8d4d420bb02dc9c4c6e52cf7a7bb32a
pkl0912 <pkl0912@naver.com>
2025-04-07 16:18:38 +0900

    chore: user-service 변경

--- user-service/src/main/java/com/ohdelivery/service/user/UserServiceApplication.java
+++ user-service/src/main/java/com/ohdelivery/service/user/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ohdelivery.service.user", "com.ohdelivery.common"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
