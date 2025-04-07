27d1e4ebd3e0a51b4a2f4ca6e0d88dc0f4407d16
pkl0912 <pkl0912@naver.com>
2025-04-07 16:18:01 +0900

    chore: exception 설정

---
+++ common/src/main/java/com/ohdelivery/common/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.ohdelivery.common.exception;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+	// 400
+	INVALID_TOKEN_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰을 입력했습니다."),
+	//404
+	USER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 유저는 존재하지 않습니다."),
+
+	// 405 METHOD_NOT_ALLOWED
+	METHOD_NOT_ALLOWED_EXCEPTION(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드 입니다."),
+
+	// 409 Conflict
+	ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "이미 존재하는 유저입니다."),
+
+	// 500
+	INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+	private final HttpStatus httpStatus;
+	private final String message;
+}
