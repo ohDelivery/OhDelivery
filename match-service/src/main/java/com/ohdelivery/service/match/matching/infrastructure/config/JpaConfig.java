936c526e88ff6d586f8522795679145675a4d555
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-04-08 11:24:30 +0900

    feat : BaseEntity 인식 위한 JpaAuditing 설정 추가

---
+++ match-service/src/main/java/com/ohdelivery/service/match/matching/infrastructure/config/JpaConfig.java
@@ -0,0 +1,16 @@
+package com.ohdelivery.service.match.matching.infrastructure.config;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("system");
+    }
+}
