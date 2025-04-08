89bbb26e28abd1a0b0b4f4368726c3fd76d7f137
sinryuji <siler2474@gmail.com>
2025-04-08 17:22:02 +0900

    feat: Delivery entity

--- delivery-service/src/main/java/com/ohdelivery/service/delivery/domain/model/Delivery.java
+++ delivery-service/src/main/java/com/ohdelivery/service/delivery/domain/model/Delivery.java
@@ -1,6 +1,53 @@
 package com.ohdelivery.service.delivery.domain.model;
 
 
-public class Delivery {
+import com.ohdelivery.common.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
+@Getter
+@Entity
+@Table(name = "p_delivery")
+@SQLRestriction("deleted_at is null")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Delivery extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Embedded
+    private OrderInfo orderInfo;
+
+    @Column(name = "target_address", nullable = false)
+    private String targetAddress;
+
+    @Embedded
+    private PathInfo pathInfo;
+
+    @Column(name = "fee", nullable = false)
+    private Integer fee;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @Column(name = "payment_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentType paymentType;
+
+    @Column(name = "payment_amount", nullable = false)
+    private Integer paymentAmount;
 }
