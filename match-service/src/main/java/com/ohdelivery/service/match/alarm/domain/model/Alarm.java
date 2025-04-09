ebc687d721911688c42b376342bca83e43e89361
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-04-10 03:14:29 +0900

    feat: Alarm-AlarmRider mapping

--- match-service/src/main/java/com/ohdelivery/service/match/alarm/domain/model/Alarm.java
+++ match-service/src/main/java/com/ohdelivery/service/match/alarm/domain/model/Alarm.java
@@ -2,14 +2,24 @@ package com.ohdelivery.service.match.alarm.domain.model;
 
 import com.ohdelivery.common.model.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "p_alarm")
 public class Alarm extends BaseEntity {
 
@@ -21,5 +31,13 @@ public class Alarm extends BaseEntity {
 
   private String message;
 
-  private String sentAt;
+  @OneToMany(mappedBy = "alarm", fetch = FetchType.LAZY)
+  private List<AlarmRider> alarmRider = new ArrayList<>();
+
+  public static Alarm toEntity(UUID deliveryId, String message) {
+    return Alarm.builder()
+        .deliveryId(deliveryId)
+        .message(message)
+        .build();
+  }
 }
