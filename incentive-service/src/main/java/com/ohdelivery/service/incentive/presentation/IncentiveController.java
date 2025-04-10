633b7f35bedb604f67a4b0d2313c3890c808eae7
pkl0912 <pkl0912@naver.com>
2025-04-10 15:42:48 +0900

    feat: 인센티브 crud 완성

--- incentive-service/src/main/java/com/ohdelivery/service/incentive/presentation/IncentiveController.java
+++ incentive-service/src/main/java/com/ohdelivery/service/incentive/presentation/IncentiveController.java
@@ -1,13 +1,24 @@
 package com.ohdelivery.service.incentive.presentation;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ohdelivery.common.kafka.dto.DeliveryIncentiveDto;
-import com.ohdelivery.service.incentive.infrastructure.messaging.KafkaProducer;
+import com.ohdelivery.common.response.ApiResponse;
+import com.ohdelivery.common.response.SuccessCode;
+import com.ohdelivery.service.incentive.application.request.IncentiveRequest;
+import com.ohdelivery.service.incentive.application.response.IncentiveResponse;
+import com.ohdelivery.service.incentive.application.service.IncentiveService;
+import com.ohdelivery.service.incentive.infrastructure.messaging.out.KafkaProducer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,10 +28,38 @@ import lombok.RequiredArgsConstructor;
 public class IncentiveController {
 
 	private final KafkaProducer kafkaProducer;
+	private final IncentiveService incentiveService;
 
 	@PostMapping
 	public ResponseEntity<String> create(@RequestBody DeliveryIncentiveDto dto) {
 		kafkaProducer.deliveryRecordEvent(dto);
 		return ResponseEntity.ok("Success");
 	}
+
+	@GetMapping("/{incentiveId}")
+	public ResponseEntity<ApiResponse<IncentiveResponse.GetIncentiveResponse>> getIncentive(
+		@PathVariable UUID incentiveId
+	) {
+		IncentiveResponse.GetIncentiveResponse response =  incentiveService.getIncentive(incentiveId);
+		return ResponseEntity.ok(ApiResponse.success(SuccessCode.INCENTIVE_GET_SUCCESS.getCode().toString(),SuccessCode.INCENTIVE_GET_SUCCESS.getMessage(),response));
+	}
+
+	@PutMapping("/{incentiveId}")
+	public ResponseEntity<ApiResponse<String>> updateIncentive(
+		@PathVariable UUID incentiveId,
+		@RequestBody IncentiveRequest.updateIncentiveRequest request
+	) {
+		incentiveService.updateIncentive(incentiveId, request);
+		return ResponseEntity.ok(ApiResponse.success(SuccessCode.INCENTIVE_UPDATED_SUCCESS.getCode().toString(),SuccessCode.INCENTIVE_UPDATED_SUCCESS.getMessage()));
+	}
+
+	@DeleteMapping("/{incentiveId}")
+	public ResponseEntity<ApiResponse<IncentiveResponse.DeleteIncentiveResponse>> deleteIncentive(
+		@PathVariable UUID incentiveId
+	) {
+
+		return ResponseEntity.ok(ApiResponse.success(SuccessCode.INCENTIVE_DELETED_SUCCESS.getCode().toString(),SuccessCode.INCENTIVE_DELETED_SUCCESS.getMessage(), incentiveService.deleteIncentive(incentiveId)));
+	}
+
+
 }
