1f2de5507c1538197228d194fd7a5e1745cc65e0
pkl0912 <pkl0912@naver.com>
2025-04-11 12:06:05 +0900

    chore: 테스트용 producer 주석 처리

--- incentive-service/src/main/java/com/ohdelivery/service/incentive/infrastructure/messaging/out/KafkaProducer.java
+++ incentive-service/src/main/java/com/ohdelivery/service/incentive/infrastructure/messaging/out/KafkaProducer.java
@@ -12,12 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class KafkaProducer {
-	private static final String TOPIC = "delivery-record-create";
-
-	private final KafkaTemplate<String, Object> kafkaTemplate;
-
-	public void deliveryRecordEvent(DeliveryIncentiveDto incentiveDto) {
-		log.info("배달기록 생성!!!!!!");
-		kafkaTemplate.send(TOPIC, incentiveDto);
-	}
+	// private static final String TOPIC = "delivery-record-create";
+	//
+	// private final KafkaTemplate<String, Object> kafkaTemplate;
+	//
+	// public void deliveryRecordEvent(DeliveryIncentiveDto incentiveDto) {
+	// 	log.info("배달기록 생성!!!!!!");
+	// 	kafkaTemplate.send(TOPIC, incentiveDto);
+	// }
 }
