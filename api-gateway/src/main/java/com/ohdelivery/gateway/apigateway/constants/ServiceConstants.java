565c16c1e538f759577a10b63e1c46a0de6d7f5d
pkl0912 <pkl0912@naver.com>
2025-04-15 11:48:08 +0900

    feat: api-gateway 에 constant 추가

--- api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
+++ api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ServiceConstants {
 
-    USER_SERVICE("user-service", "lb://user-service", new String[]{"/users/**"});
+    USER_SERVICE("user-service", "lb://user-service", new String[]{"/users/**"}),
+    DELIVERY_SERVICE("delivery-service", "lb://delivery-service", new String[]{"/deliveries/**"}),
+    INCENTIVE_SERVICE("incentive-service", "lb://incentive-service", new String[]{"/incentives/**"});
 
     private final String serviceName;
     private final String serviceUri;
