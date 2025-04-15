b8f25f82fb9f31eb48938aeda63ff4a26fb6baa3
kade <bhpark93@gmail.com>
2025-04-15 12:32:58 +0900

    fix: 토큰 인증 예외처리 추가

--- api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/filter/TokenFilter.java
+++ api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/filter/TokenFilter.java
@@ -4,14 +4,20 @@ import static com.ohdelivery.gateway.apigateway.constants.FilterConstants.AUTHOR
 import static com.ohdelivery.gateway.apigateway.constants.FilterConstants.BEARER_PREFIX;
 import static com.ohdelivery.gateway.apigateway.constants.FilterConstants.PASSPORT_ATTRIBUTE;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ohdelivery.common.passport.Passport;
+import com.ohdelivery.common.response.ApiResponse;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
@@ -24,6 +30,7 @@ public class TokenFilter implements GatewayFilter {
 
     @Value("${auth-server.validate-url}")
     private String validateUrl;
+
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -40,8 +47,14 @@ public class TokenFilter implements GatewayFilter {
                 .uri(validateUrl)
                 .bodyValue(token)
                 .retrieve()
+                .onStatus(status -> status == HttpStatus.UNAUTHORIZED || status == HttpStatus.BAD_REQUEST ||status == HttpStatus.FORBIDDEN,
+                        response -> response.bodyToMono(ApiResponse.class)
+                                .flatMap(r ->{
+                                    return writeErrorResponse(exchange, r);
+                                })
+                )
                 .onStatus(HttpStatusCode::isError,
-                        response -> Mono.error(new RuntimeException("인증에 실패했습니다.")))
+                        response -> Mono.error(new RuntimeException("인증 과정에 오류가 발생했습니다.")))
                 .bodyToMono(Passport.class)
                 .flatMap(passport -> {
                     exchange.getAttributes().put(PASSPORT_ATTRIBUTE, passport);
@@ -52,5 +65,20 @@ public class TokenFilter implements GatewayFilter {
     private Mono<Void> unauthorizedResponse(ServerHttpResponse response) {
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         return response.setComplete();
+    }
+
+    private Mono<Void> writeErrorResponse(ServerWebExchange exchange, ApiResponse<Void> response) {
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper mapper = new ObjectMapper();
+        byte[] bytes;
+        try {
+            bytes = mapper.writeValueAsBytes(response);
+        } catch (JsonProcessingException e) {
+            bytes = "{\"오류 정보 추출 중 오류가 발생했습니다.\"}".getBytes(StandardCharsets.UTF_8);
+        }
+
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+        return exchange.getResponse().writeWith(Mono.just(buffer));
     }
 }
