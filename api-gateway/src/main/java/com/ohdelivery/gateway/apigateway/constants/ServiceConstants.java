787ec7170ad43d629b1eced6b0fb660fcc6a84a3
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-04-15 16:40:18 +0900

    refactor : apiGateway에 match-service추가

--- api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
+++ api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ServiceConstants {
 
-    USER_SERVICE("user-service", "lb://user-service", new String[]{"/api/users"}),
-    AUTH_SERVER("auth-server", "lb://auth-server", new String[]{"/auth/logout"})
-    ;
+  USER_SERVICE("user-service", "lb://user-service", new String[]{"/api/users"}),
+  AUTH_SERVER("auth-server", "lb://auth-server", new String[]{"/auth/logout"}),
+  MATCHING_SERVICE("match-service", "lb://match-service", new String[]{"/api/*"});
 
-    private final String serviceName;
-    private final String serviceUri;
-    private final String[] apiPaths;
+  private final String serviceName;
+  private final String serviceUri;
+  private final String[] apiPaths;
 
 }
