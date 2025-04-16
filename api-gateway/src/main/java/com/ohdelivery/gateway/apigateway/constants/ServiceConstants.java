019a8912d4d5a8ce99229a98c54ca6a577c6d6d1
pkl0912 <pkl0912@naver.com>
2025-04-16 11:56:11 +0900

    feat: rolecheck 추가

--- api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
+++ api-gateway/src/main/java/com/ohdelivery/gateway/apigateway/constants/ServiceConstants.java
@@ -11,7 +11,6 @@ public enum ServiceConstants {
     INCENTIVE_SERVICE("incentive-service", "lb://incentive-service", new String[]{"/incentives/**"}),
     USER_SERVICE("user-service", "lb://user-service", new String[]{"/api/users"}),
     AUTH_SERVER("auth-server", "lb://auth-server", new String[]{"/auth/logout"}),
-    DELIVERY_SERVICE("delivery-service", "lb://delivery-service", new String[]{"/deliveries/**"});
     MATCHING_SERVICE("match-service", "lb://match-service", new String[]{"/api/*"});
 
   private final String serviceName;
