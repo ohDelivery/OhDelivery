5e757d48d248bbfeec999611ce481f0e50da23fb
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2025-04-20 14:39:35 +0900

    refactor: agent repository 추상화

--- consult-service/src/main/java/com/ohdelivery/service/consult/agent/domain/repository/AgentRepository.java
+++ consult-service/src/main/java/com/ohdelivery/service/consult/agent/domain/repository/AgentRepository.java
@@ -2,10 +2,10 @@ package com.ohdelivery.service.consult.agent.domain.repository;
 
 import com.ohdelivery.service.consult.agent.domain.model.Agent;
 import java.util.Optional;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgentRepository extends JpaRepository<Agent, UUID> {
+public interface AgentRepository {
+
+  Agent save(Agent agent);
 
   Optional<Agent> findByAgentId(Long agentId);
 }
