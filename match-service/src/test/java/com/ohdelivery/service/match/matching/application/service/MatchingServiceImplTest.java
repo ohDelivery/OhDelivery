45094fae5b53990d099321a957fc87dbfe4d1886
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-04-28 23:43:06 +0900

    fix : 오류 수정

--- match-service/src/test/java/com/ohdelivery/service/match/matching/application/service/MatchingServiceImplTest.java
+++ match-service/src/test/java/com/ohdelivery/service/match/matching/application/service/MatchingServiceImplTest.java
@@ -23,8 +23,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)  // lenient 설정
 class MatchingServiceImplTest {
 
   @Mock
