9175d1d83e19f1814c055766d3b6ed9663749d16
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-04-30 16:58:56 +0900

    feat : aws에 올리는용 수정

--- match-service/src/main/java/com/ohdelivery/service/match/common/redisson/RedissonConfig.java
+++ match-service/src/main/java/com/ohdelivery/service/match/common/redisson/RedissonConfig.java
@@ -3,17 +3,27 @@ package com.ohdelivery.service.match.common.redisson;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+  @Value("${spring.data.redis.host}")
+  String host;
+
+  @Value("${spring.data.redis.port}")
+  String port;
+
   @Bean
   public RedissonClient redissonClient() {
     Config config = new Config();
+
+    String address = "redis://" + host + ":" + port;
+
     config.useSingleServer()
-        .setAddress("redis://localhost:6379") // Redis 서버 주소 설정
+        .setAddress(address) // Redis 서버 주소 설정
         .setPassword("root"); // Redis 비밀번호
     return Redisson.create(config);
   }
